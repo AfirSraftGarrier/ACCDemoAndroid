70d0cbb6d9a46f18c1491894512c92c970e1690e
AfirSraftGarrier <AfirSraftGarrier@gmail.com>
2016-11-28 23:18:33 +0800

    To update project.

---
+++ app/src/main/java/afirsraftgarrier/demoandroid/base/DemoDescriper.java
@@ -0,0 +1,13 @@
+package afirsraftgarrier.demoandroid.base;
+
+/**
+ * Created by AfirSraftGarrier on 2016/11/11 0011.
+ */
+
+public interface DemoDescriper {
+    String getDemoName();
+
+    Class getDemoClass();
+
+    String getDemoDescription();
+}
---
+++ app/src/main/java/afirsraftgarrier/demoandroid/framework/MaterialDesignActivity.java
@@ -0,0 +1,17 @@
+package afirsraftgarrier.demoandroid.framework;
+
+import android.app.Activity;
+import android.content.SyncStatusObserver;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+/**
+ * Created by AfirSraftGarrier on 2016/11/12 0012.
+ */
+
+public class MaterialDesignActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
