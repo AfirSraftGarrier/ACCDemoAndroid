8696e4e451fa016350963a2bc4d00ea63e91e9ba
AfirSraftGarrier <AfirSraftGarrier@gmail.com>
2016-12-14 19:36:55 +0800

    Add pay demo.

---
+++ app/src/main/java/afirsraftgarrier/demoandroid/thirdparty/perform/PerformFixActivity.java
@@ -0,0 +1,20 @@
+package afirsraftgarrier.demoandroid.thirdparty.perform;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import afirsraftgarrier.demoandroid.thirdparty.pay.lib.weixin.WXPay;
+
+public class PerformFixActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View v) {
+    }
+
+}
